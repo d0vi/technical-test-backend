17b0daacd05c312fef4343fee588d0948ea21ede
Jesús Iglesias Iglesias <dovimeister@gmail.com>
2025-07-22 23:25:44 +0200

    🧽 Refactor Stripe service class

--- src/main/java/com/playtomic/tests/wallet/infrastructure/adapter/driven/provider/stripe/StripeService.java
+++ src/main/java/com/playtomic/tests/wallet/infrastructure/adapter/driven/provider/stripe/StripeService.java
@@ -7,12 +7,10 @@ import java.net.URI;
 import lombok.NonNull;
 import org.springframework.web.client.RestTemplate;
 
-/**
- * Handles the communication with Stripe.
- *
- * <p>A real implementation would call to Stripe using their API/SDK. This dummy implementation
- * throws an error when trying to charge less than 10€.
- */
+/// Handles the communication with Stripe.
+///
+/// A real implementation would call to Stripe using their API/SDK. This dummy implementation
+/// throws an error when trying to charge less than 10€.
 public class StripeService implements PaymentService {
 
   private final URI chargesUri;
@@ -25,29 +23,27 @@ public class StripeService implements PaymentService {
     this.restTemplate = restTemplate;
   }
 
-  /**
-   * Charges money in the credit card.
-   *
-   * <p>Ignore the fact that no CVC or expiration date are provided.
-   *
-   * @param creditCardNumber The number of the credit card
-   * @param amount The amount that will be charged.
-   * @throws StripeServiceException if the amount is too low
-   */
+  /// Charges money in the credit card.
+  ///
+  /// Ignore the fact that no CVC or expiration date are provided.
+  ///
+  /// @param creditCardNumber The number of the credit card
+  /// @param amount The amount that will be charged.
+  /// @throws StripeServiceException if the amount is too low
   @Override
   public String charge(@NonNull String creditCardNumber, @NonNull BigDecimal amount)
       throws StripeServiceException {
     Payment payment =
-        restTemplate.postForObject(
-            chargesUri, new ChargeRequest(creditCardNumber, amount), Payment.class);
-    return payment.id();
+        this.restTemplate.postForObject(
+            this.chargesUri, new ChargeRequest(creditCardNumber, amount), Payment.class);
+    return payment != null ? payment.id() : null;
   }
 
-  /** Refunds the specified payment. */
+  /// Refunds the specified payment.
   @Override
   public void refund(@NonNull String paymentId) throws StripeServiceException {
     // Object.class because we don't read the body here.
-    restTemplate.postForEntity(refundsUri.toString(), null, Object.class, paymentId);
+    this.restTemplate.postForEntity(this.refundsUri.toString(), null, Object.class, paymentId);
   }
 
   private record ChargeRequest(
